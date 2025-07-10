0e93bba9506769c86b52811c37a47ee00ed29c21
pavan <pavanvenkatanagamanoj@gmail.com>
2025-07-10 19:34:18 +0530

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Adrian JSM
+          Pavan Bollepalli
         </a>
 
         <nav className="desktop">
